31f81ae4695d5dee4a335001e8dd7682859108e3
hehaosen <307052084@qq.com>
2016-06-12 12:14:44 +0800

    完整版

--- reload-back.js
+++ reload-back.js
@@ -1,5 +1,5 @@
 /*
- * 返回页面时刷新 v0.0.1
+ * 返回页面时刷新 v0.1.0
  * Date: 2016-6-3
  * author:大力神
  * http://www.github.com/hehaosen
@@ -9,73 +9,13 @@ var reloadBack = function () {
 
     var _self = this ;
 
-    // 分隔符
-
-    var __SEPARATOR__ = '|||';
-
-    /**
-     * 获取已经存储的url
-     * @returns {string|*}
-     */
-
-    function getUrlTeam () {
-
-        if ( window.localStorage ) {
-            return localStorage.reloadBackUrl ? localStorage.reloadBackUrl : '';
-        } else {
-            // 执行cookie获取
-        }
-    }
-
-    /**
-     * 获取回退几页前的url
-     * @param step
-     * @returns {*}
-     */
-
-    function getBackUrl ( step ) {
-
-        step = step ? step : 0 ;
-
-        return getUrlTeam().split(__SEPARATOR__)
-            [getUrlTeam().split(__SEPARATOR__).length - 2 - step];
-    }
-
-    /**
-     * 返回页面
-     * @param step 不传值时返回上一页
-     */
-
-    _self.goBack = function ( step ) {
-
-        var hrefUrl = getBackUrl(step);
-
-        if ( hrefUrl ) {
-            window.location.href = hrefUrl;
-        } else {
-            console.error('无法获取上一级页面');
-        }
-    };
-
     /**
      * 存储当前url,用于为以后跳转
      */
 
     _self.addUrl = function () {
         // 当页面刷新时,不记录url地址
-        if (getBackUrl() != window.location.href) {
-            localStorage.reloadBackUrl = localStorage.reloadBackUrl ? localStorage.reloadBackUrl : '';
-            localStorage.reloadBackUrl += window.location.href + __SEPARATOR__;
-        }
-    };
-
-    /**
-     * 清除url地址
-     */
-    _self.clearUrl = function () {
-        if ( window.localStorage ) {
-            window.localStorage.removeItem('reloadBackUrl');
-        }
+        window.localStorage.nowUrl = window.location.href;
     };
 
     /**
@@ -84,10 +24,8 @@ var reloadBack = function () {
     _self.listenerBack = function () {
 
         var interEvent  = function () {
-
-            console.log (getBackUrl(-1));
-            if ( getBackUrl(-1) != window.location.href && getBackUrl(-1) != '') {
-                console.log('刷新刷新!!!!');
+            if ( window.localStorage.nowUrl != window.location.href) {
+                location.reload();
             }
         };
 
@@ -96,3 +34,4 @@ var reloadBack = function () {
 };
 reloadBack = new reloadBack();
 reloadBack.addUrl();
+reloadBack.listenerBack();
