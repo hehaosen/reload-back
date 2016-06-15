f9f6a1f7078e49fab5b76c96d6c8dd69d694b726
hehaosen <307052084@qq.com>
2016-06-15 09:24:42 +0800

    +cookie操作方法

--- reload-back.js
+++ reload-back.js
@@ -46,6 +46,48 @@ var reloadBack = function () {
 
         setInterval(interEvent, 100);
     };
+
+    /**
+     *  写cookie
+      * @param name
+     * @param value
+     */
+
+    function setCookie(name,value)
+    {
+        var Days = 30;
+        var exp = new Date();
+        exp.setTime(exp.getTime() + Days*24*60*60*1000);
+        document.cookie = name + "="+ value + ";expires=" + exp.toGMTString();
+    }
+
+    /**
+     * 删除cookie
+     * @param name
+     */
+
+    function delCookie(name)
+    {
+        var exp = new Date();
+        exp.setTime(exp.getTime() - 1);
+        var cval=getCookie(name);
+        if(cval!=null)
+            document.cookie= name + "="+cval+";expires="+exp.toGMTString();
+    }
+
+    /**
+     * 读取cookie
+     * @param name
+     * @returns {null}
+     */
+    function getCookie(name)
+    {
+        var arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
+        if(arr=document.cookie.match(reg))
+            return unescape(arr[2]);
+        else
+            return null;
+    }
 };
 reloadBack = new reloadBack();
 reloadBack.addUrl();
